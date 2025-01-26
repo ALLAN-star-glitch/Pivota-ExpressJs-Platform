92ae44b120c4504d70a5b28a245b15ca19e2f4f6
Allan Mathenge <allanmathenge67@gmail.com>
2025-01-26 17:44:21 +0300

    corrected the sign up page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ const SignUp = () => {
 
   const router = useRouter();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
     e.preventDefault();
 
     // Reset errors
